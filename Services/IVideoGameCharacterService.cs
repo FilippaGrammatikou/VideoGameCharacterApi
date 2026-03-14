eda914ee3043af6763abb91bda51089e412d6d35
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-14 19:28:26 +0200

    feat(services): define nullable character lookup by id in service contract

--- Services/IVideoGameCharacterService.cs
+++ Services/IVideoGameCharacterService.cs
@@ -5,7 +5,7 @@ namespace VideoGameCharacterApi.Services
     {
         // Add CRUD Methods within the interface
         Task<List<Character>> GetAllCharactersAsync();
-        Task<Character> GetCharacterByIdAsync(int id);
+        Task<Character?> GetCharacterByIdAsync(int id);
         Task<Character> AddCharacterAsync(Character character);
         Task<bool> UpdateCharacterAsync(int id, Character character);
         Task<bool> DeleteCharacterAsynch(int id);
