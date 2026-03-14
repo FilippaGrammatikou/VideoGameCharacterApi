dd308c0a47a6504fa678719142d331724936b46e
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-14 18:32:05 +0200

    feat(services): add CRUD method signatures to character service interface

--- Services/IVideoGameCharacterService.cs
+++ Services/IVideoGameCharacterService.cs
@@ -1,7 +1,13 @@
-﻿namespace VideoGameCharacterApi.Services
+﻿using VideoGameCharacterApi.Models;
+namespace VideoGameCharacterApi.Services
 {
     public interface IVideoGameCharacterService
     {
-
+        // Add CRUD Methods within the interface
+        Task<List<Character>> GetAllCharactersAsync();
+        Task<Character> GetCharacterByIdAsync(int id);
+        Task<Character> AddCharacterAsync(Character character);
+        Task<bool> UpdateCharacterAsync(int id, Character character);
+        Task<bool> DeleteCharacterAsynch(int id);
     }
 }
