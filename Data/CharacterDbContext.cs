b3acdb76d39052ad28b22dfc4b273eea7850fab2
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-16 19:17:36 +0200

    feat(data): add Characters DbSet to CharacterDbContext

--- Data/CharacterDbContext.cs
+++ Data/CharacterDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterApi.Models;
 
 namespace VideoGameCharacterApi.Data
 {
-    public class CharacterDbContext(DbContextOptions<CharacterDbContext> options) : DbContext
+    public class CharacterDbContext(DbContextOptions<CharacterDbContext> options) : DbContext(options)
     {
-
+        public DbSet<Character> Characters => Set<Character>();
     }
 }
