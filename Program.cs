db1dac28423b710b371de150e9078c833178ace2
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-17 12:46:20 +0200

    feat(data): configure CharacterDbContext to use the SQL Server connection string

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using VideoGameCharacterApi.Data;
 using VideoGameCharacterApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add services to the container.
 builder.Services.AddControllers();
 
 //OpenAPI structure for eg. documentations
 builder.Services.AddOpenApi();
-// Register character service for dependency injection (one instance per request).
+
+//Add the database context to the application and tell Entity Framework to connect to SQL Server
+builder.Services.AddDbContext<CharacterDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Register character service for dependency injection (one instance per request).
 builder.Services.AddScoped<IVideoGameCharacterService, VideoGameService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -21,9 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
