b32d2ed5a7be545f2fb24f9b63be5721f4d72f32
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-17 19:12:49 +0200

    feat(services): update service contract to use CreateCharacterRequest and UpdateCharacterRequest

--- Services/IVideoGameCharacterService.cs
+++ Services/IVideoGameCharacterService.cs
@@ -9,8 +9,8 @@ namespace VideoGameCharacterApi.Services
         //Return DTOs instead of entity models in API responses
         Task <List<CharacterResponseDto>> GetAllCharactersAsync();
         Task<CharacterResponseDto?> GetCharacterByIdAsync(int id);
-        Task<CharacterResponseDto> AddCharacterAsync(Character character);
-        Task<bool> UpdateCharacterAsync(int id, Character character);
+        Task<CharacterResponseDto> AddCharacterAsync(CreateCharacterRequest character);
+        Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequest character);
         Task<bool> DeleteCharacterAsynch(int id);
     }
 }
