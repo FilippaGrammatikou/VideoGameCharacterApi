590efff196f29a8f56b2ace7660d841719213da4
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-17 18:45:34 +0200

    feat(services): return CharacterResponseDto from GetAllCharactersAsync and GetCharacterByIdAsync

--- Services/VideoGameService.cs
+++ Services/VideoGameService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using VideoGameCharacterApi.Data;
+using VideoGameCharacterApi.Dtos;
 using VideoGameCharacterApi.Models;
 
 namespace VideoGameCharacterApi.Services
@@ -8,7 +9,7 @@ namespace VideoGameCharacterApi.Services
     //Uses CharacterDbContext to read and later modify character data in SQL Server
     public class VideoGameService(CharacterDbContext _context) : IVideoGameCharacterService
     {
-        public Task<Character> AddCharacterAsync(Character character)
+        public Task<CharacterResponseDto> AddCharacterAsync(Character character)
         {
             throw new NotImplementedException();
         }
@@ -19,13 +20,27 @@ namespace VideoGameCharacterApi.Services
         }
 
         //Queries the Characters table and returns every stored character
-        public async Task<List<Character>> GetAllCharactersAsync()
-           => await _context.Characters.ToListAsync();
+        public async Task<List<CharacterResponseDto>> GetAllCharactersAsync()
+           => await _context.Characters.Select(c=>new CharacterResponseDto
+           {
+               Name =c.Name,
+               Game = c.Game,
+               Role = c.Role
+           }).ToListAsync();
 
         //Returns a single character by id, or null if it does not exist
-        public async Task<Character?> GetCharacterByIdAsync(int id)
+        public async Task<CharacterResponseDto?> GetCharacterByIdAsync(int id)
         {
-            var result = await _context.Characters.FindAsync(id);
+            var result = await _context.Characters
+                .Where(c => c.Id == id)
+                .Select(c => new CharacterResponseDto
+                {
+                    Name=c.Name,
+                    Game=c.Game,
+                    Role=c.Role
+                })
+                .FirstOrDefaultAsync();
+
             return result;
         }
 
