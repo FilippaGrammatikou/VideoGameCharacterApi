aa2459a2c5083eb6811c0a3f1444354b03232bb4
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-18 17:53:14 +0200

    refactor: remove Id from update request dto

--- Dtos/UpdateCharacterRequest.cs
+++ Dtos/UpdateCharacterRequest.cs
@@ -2,7 +2,6 @@
 {
     public class UpdateCharacterRequest
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Game { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
