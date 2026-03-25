ab1d3b2f269581898745f879e597d3728548fe60
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-25 20:47:44 +0200

    refactor(data): simplify CharacterDbContext by converting the Characters DbSet to an expression-bodied property

--- Data/CharacterDbContext.cs
+++ Data/CharacterDbContext.cs
@@ -3,8 +3,10 @@ using VideoGameCharacterApi.Models;
 
 namespace VideoGameCharacterApi.Data
 {
+    //Database context used by EF Core to query and persist application data.
     public class CharacterDbContext(DbContextOptions<CharacterDbContext> options) : DbContext(options)
     {
+        //Provides access to Character entities stored in the database.
         public DbSet<Character> Characters => Set<Character>();
     }
 }
