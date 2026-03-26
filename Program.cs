c2690810dea1cba5ec0d521637528717ff1127c0
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-26 19:43:45 +0200

    feat: configure JWT bearer authentication and role-based authorization

--- Program.cs
+++ Program.cs
@@ -3,8 +3,40 @@ using Scalar.AspNetCore;
 using VideoGameCharacterApi.Data;
 using VideoGameCharacterApi.Infrastructure;
 using VideoGameCharacterApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+var jwtIssuer = builder.Configuration["Jwt:Issuer"]!;
+var jwtAudience = builder.Configuration["Jwt:Audience"]!;
+
+//configure how the API should read and verify incoming JWT tokens
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    //configuration object containing all the checks the API will perform against a token
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, //Check who issued the token
+        ValidateAudience = true, //Check who the token was intended for
+        ValidateIssuerSigningKey = true, //Verify the token’s signature was created using the correct secret key
+        ValidateLifetime = true, //Check whether the token has expired
+        ValidIssuer = jwtIssuer, //This is the exact issuer value expected
+        ValidAudience = jwtAudience, //This is the exact audience value I expect
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)) //Use this shared secret key to verify that the token signature is real
+    };                                                                                                                                  //Convert secret text into bytes
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserOrAdmin", policy =>
+        policy.RequireRole("User", "Admin"));
+
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireRole("Admin"));
+});
 
 //Add services to the container.
 builder.Services.AddControllers();
@@ -36,6 +68,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseExceptionHandler();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
