e227c728e866a9f6c95498e5932b101ab070c40e
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-27 13:08:17 +0200

    AuthController Initilization and SetUp

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using VideoGameCharacterApi.Dtos;
+
+namespace VideoGameCharacterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IConfiguration configuration) : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost("loggin")]
+        public ActionResult<LoginResponse> Login(LoginRequest request)
+        {
+
+
+
+            return Unauthorized();
+        }
+    }
+}
