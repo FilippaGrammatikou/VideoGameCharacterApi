acc06b2bfb1f3f259156947801fa38c66c309e94
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-29 17:14:08 +0300

    feat: create GetCharactersQuery DTO for character list query parameters

---
+++ Dtos/GetCharactersQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoGameCharacterApi.Dtos
+{
+    public class GetCharactersQuery
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? Game { get; set; }
+        public string? Role { get; set; }
+        public string? SortBy { get; set; }
+        public string? SortDirection { get; set; } = "asc";
+    }
+}
