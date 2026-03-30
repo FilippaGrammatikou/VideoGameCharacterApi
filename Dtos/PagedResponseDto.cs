49a7de9da31bea45b293f2de1a26bc60425333cd
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-30 16:52:46 +0300

    feat: create PagedResponseDto to standardize paginated responses

---
+++ Dtos/PagedResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VideoGameCharacterApi.Dtos
+{
+    public class PagedResponseDto<T>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }  //mtching items of the search
+        public List<T> Items { get; set; } = [];  //tells client about page metadata & returns actual characters for the page
+    }
+}
