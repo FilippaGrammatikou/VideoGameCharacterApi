dd5305ef1e6a1dd955adca8aed5c825db691443e
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-03-30 17:10:51 +0300

    refactor: change GetAllCharactersAsync to use query-based pagination contract

--- Services/IVideoGameCharacterService.cs
+++ Services/IVideoGameCharacterService.cs
@@ -7,7 +7,7 @@ namespace VideoGameCharacterApi.Services
     {
         // Add CRUD Methods within the interface
         //Return DTOs instead of entity models in API responses
-        Task <List<CharacterResponseDto>> GetAllCharactersAsync();
+        Task <PagedResponseDto<CharacterResponseDto>> GetAllCharactersAsync(GetCharactersQuery query); //get a queried page of characters based on filters/sort/pagination
         Task<CharacterResponseDto?> GetCharacterByIdAsync(int id);
         Task<CharacterResponseDto> AddCharacterAsync(CreateCharacterRequest character);
         Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequest character);
