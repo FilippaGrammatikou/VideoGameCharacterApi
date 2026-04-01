2c25259c2394112e22a91780ebc757370563d983
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-04-01 16:34:07 +0300

    test: create CustomWebApplicationFactory to bootstrap the API in tests

---
+++ VideoGameCharacterApi.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+//This is the reusable object that boots the app for integration tests
+namespace VideoGameCharacterApi.Tests;
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+}
