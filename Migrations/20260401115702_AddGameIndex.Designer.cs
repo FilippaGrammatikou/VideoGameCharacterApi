7a24a28a050e0575c61e92854dbd609d0fa47adb
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-04-01 14:57:37 +0300

    feat(database): add migration for Character.Game index to support query performance

--- Migrations/20260401115702_AddGameIndex.Designer.cs
+++ Migrations/20260401115702_AddGameIndex.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VideoGameCharacterApi.Data;
 
@@ -10,9 +11,11 @@ using VideoGameCharacterApi.Data;
 namespace VideoGameCharacterApi.Migrations
 {
     [DbContext(typeof(CharacterDbContext))]
-    partial class CharacterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260401115702_AddGameIndex")]
+    partial class AddGameIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260401115702_AddGameIndex.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoGameCharacterApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Game",
+                table: "Characters",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_Game",
+                table: "Characters",
+                column: "Game");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_Game",
+                table: "Characters");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Game",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
